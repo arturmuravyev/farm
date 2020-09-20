69b7ea6478440e5f707d69c03ddf7499cd73116f
Art Murr <12go12go@mail.ru>
2020-09-21 02:16:31 +0300

    ui v1

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+import Vuetify from 'vuetify';
+
+window.Vue = require('vue');
+
+Vue.use(Vuetify);
+
+Vue.component('front-page', require('./components/Front.vue').default);
+
+const vuetifyOptions={};
+const app = new Vue({
+	el: '#app',
+	vuetify: new Vuetify(vuetifyOptions),
+});
---
+++ resources/js/components/Front.vue
@@ -0,0 +1,50 @@
+<template>
+	<!-- <v-app> -->
+		<div class="container m-5">
+		    <button v-on:click="loadAnimalKinds" type="button" class="btn btn-primary btn-fab">
+				Get Animal Kinds
+			</button>
+			<div class="d-inline" v-for="kind in animalKinds">
+				<img :src="getPic(kind)" v-on:click="grow(kind)" height="32px" class="ml-3">
+			</div>
+		</div>
+	<!-- </v-app> -->
+</template>
+
+<script>
+	export default {
+		data: function() {
+			return {
+				animalKinds: []
+			}
+		},
+
+		mounted() {
+			//this.loadAnimalKinds();
+		},
+
+		methods: {
+
+			getPic: function(kind){
+				return 'images/' + kind.kind + '.png';
+				console.log('Greet');
+			},
+
+			loadAnimalKinds: function() {
+				// load API
+				axios.get('/api/animal_kinds')
+				.then( (response) => {
+					console.log('Response = ', response);
+					this.animalKinds = response.data.data;
+				})
+				.catch(function(error){
+					console.log(error);					
+				});
+			},
+
+			grow: function(kind) {
+
+			}
+		}
+	}
+</script>
